87846d45290b295103f0f899eb000c83f468e23d
Felipe Vilela <felipe.vilela01@outlook.com>
2025-09-05 21:32:01 -0300

    Logs Logcat

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/cdv/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/cdv/MainActivity.kt
@@ -1,10 +1,8 @@
 package br.edu.ifsp.scl.ads.prdm.sc303898x.cdv
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import br.edu.ifsp.scl.ads.prdm.sc303898x.cdv.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,26 +13,40 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+        Log.v(getString(R.string.app_name), "onCreate: Iniciando cliclo de vida COMPLETO")
 
     }
 
     override fun onStart() {
         super.onStart()
+        Log.v(getString(R.string.app_name), "onStart: Iniciando cliclo de vida VISIVEL")
+
     }
 
     override fun onResume() {
         super.onResume()
+        Log.v(getString(R.string.app_name), "onResume: Iniciando cliclo de vida EM FOREGROUND")
     }
 
     override fun onPause() {
         super.onPause()
+        Log.v(getString(R.string.app_name), "onPause: Finalizando cliclo de vida FOREGROUND")
+
     }
 
     override fun onStop() {
         super.onStop()
+        Log.v(getString(R.string.app_name), "onStop: Finalizando cliclo de vida VISIVEL")
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.v(getString(R.string.app_name), "onDestroy: Finalizando cliclo de vida COMPLETO")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.v(getString(R.string.app_name), "onRestart: Preparando execução do onStart")
     }
 }
